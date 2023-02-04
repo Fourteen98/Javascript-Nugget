e34d5cbd74f2a3b1e7156db88b5ade0e5d4f5b27
Fourteen98 <emuhiydeen6@gmail.com>
2023-02-04 23:35:45 +0000

    add reduce

---
+++ reduce/app.js
@@ -0,0 +1,15 @@
+// Reduce
+
+const people = [
+  { name: 'bob', age: 20, position: 'developer', salary: 100 },
+  { name: 'peter', age: 25, position: 'designer', salary: 300 },
+  { name: 'susy', age: 30, position: 'the boss', salary: 400 },
+  { name: 'anna', age: 35, position: 'intern', salary: 10 },
+];
+
+const dailyTotal = people.reduce((acc, curr) => {
+  acc += curr.salary;
+  return acc;
+}, 0)
+
+console.log(dailyTotal);
