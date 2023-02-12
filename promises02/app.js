647ce77427c697386d1087dbdde56964c8627f5f
Fourteen98 <emuhiydeen6@gmail.com>
2023-02-12 12:42:42 +0000

    add promises02

---
+++ promises02/app.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  addColor(1000, '.first', 'red').then(() => {
+    addColor(3000, '.second', 'blue').then(() => {
+      addColor(5000, '.third', 'green').catch((e) => {
+        console.log(e);
+      })
+    }).catch((e) => {
+      console.log(e);
+    })
+  }).catch((e) => console.log(e))
+  
+});
+
+function addColor(time, selector, color) {
+  const element = document.querySelector(selector);
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color;
+        resolve();
+      }, time);
+    } else {
+      reject(`There is no such element:"${element}"`);
+    }
+  })
+}
